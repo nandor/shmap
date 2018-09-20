74e77dc2248bdc10655407b76d925264fd7bbb7d
Nandor Licker <n@ndor.email>
2018-09-20 12:19:35 +0100

    Plugged in shmap.

--- src/worker/cache.ml
+++ src/worker/cache.ml
@@ -1,30 +1,41 @@
 
 open Compile_common
 
+let key = 1234
+let addr = 1 lsl 40
+let keys = 16 * 1024
+let size = 4 * 1024   (* 4Gb *)
 
-let cache = Hashtbl.create 128
+let cache = Shmap.create ~key ~addr ~keys ~size
+
+let make_key bucket key =
+  let digest = Digest.string (bucket ^ key) in
+  let rec loop i acc = match i with
+    | 0 -> acc
+    | x -> loop (x - 1) ((acc lsl 8) + (Char.code (digest.[i])))
+  in loop 8 0
 
 let get bucket key default =
   try
-    let value = Hashtbl.find cache (bucket, key) in
-    if key = "type-impl" then Hashtbl.remove cache (bucket, key);
-    Obj.obj value
+    Obj.obj (Shmap.get cache (make_key bucket key))
   with Not_found ->
     let value = default () in
-    Hashtbl.add cache (bucket, key) (Obj.repr value);
+    Shmap.set cache (make_key bucket key) (Obj.repr value);
     value
 
 let set bucket key value =
-  Hashtbl.replace cache (bucket, key) (Obj.repr value)
+  Shmap.set cache (make_key bucket key) (Obj.repr value)
 
 (* Parse tree cache *)
 let parse_impl ~tool_name ~preprocessor ~all_ppx sourcefile =
   get "ast-impl" sourcefile (fun () ->
-    Pparse.parse_implementation ~tool_name ~preprocessor ~all_ppx sourcefile)
+    Pparse.parse_implementation ~tool_name ~preprocessor ~all_ppx sourcefile
+  )
 
 let parse_intf ~tool_name ~preprocessor ~all_ppx sourcefile =
   get "ast-intf" sourcefile (fun () ->
-    Pparse.parse_interface ~tool_name ~preprocessor ~all_ppx sourcefile)
+    Pparse.parse_interface ~tool_name ~preprocessor ~all_ppx sourcefile
+  )
 
 (* Type information cache *)
 let typecheck_impl info ast =
@@ -34,7 +45,14 @@ let typecheck_impl info ast =
         info.modulename
         info.env ast
     in
-    { structure; coercion; signature; imports = Env.imports () })
+    { structure; coercion; signature; imports = Env.imports () }
+  )
+
+(* Cmx cache *)
+let () =
+  let old_read_unit_info = !Compilenv.read_unit_info in
+  Compilenv.read_unit_info := (fun filename ->
+    get "cmx" filename (fun () -> old_read_unit_info filename))
 
 (* Cmi cache *)
 let read_signature ~filename =
@@ -42,12 +60,6 @@ let read_signature ~filename =
 
 let output_cmi_hook filename cmi =
   set "cmi" filename cmi
-
-(* Cmx cache *)
-let () =
-  let old_read_unit_info = !Compilenv.read_unit_info in
-  Compilenv.read_unit_info := (fun filename ->
-    get "cmx" filename (fun () -> old_read_unit_info filename))
 
 let () =
   Env.Persistent_signature.read := read_signature;
